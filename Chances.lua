19f6285de19a3990745f06dbf4155e1d0f0b48d2
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-08-20 16:40:32 -0700

    Create Chances.lua

---
+++ Chances.lua
@@ -0,0 +1,6 @@
+{
+	["Owner"] = { Chance = 0, Rarity = Rolls.Rarity.Owner },
+	["Developer"] = { Chance = 0, Rarity = Rolls.Rarity.Developer },
+	["Test"] = { Chance = 1, Rarity = Rolls.Rarity.Common },
+	["Test2"] = { Chance = 100, Rarity = Rolls.Rarity.Rare },
+}
